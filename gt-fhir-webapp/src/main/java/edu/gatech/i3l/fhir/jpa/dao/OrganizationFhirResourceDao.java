1fbe8e5bc53313b213cc467c01eda41b1db51223
sptrakesh <sg948bc9>
2017-05-10 14:12:36 -0500

    Work in progress on supporting organization search

--- gt-fhir-webapp/src/main/java/edu/gatech/i3l/fhir/jpa/dao/OrganizationFhirResourceDao.java
+++ gt-fhir-webapp/src/main/java/edu/gatech/i3l/fhir/jpa/dao/OrganizationFhirResourceDao.java
@@ -2,9 +2,18 @@ package edu.gatech.i3l.fhir.jpa.dao;
 
 import ca.uhn.fhir.model.dstu2.resource.Organization;
 import edu.gatech.i3l.fhir.dstu2.entities.CareSite;
+import edu.gatech.i3l.fhir.jpa.entity.IResourceEntity;
 import edu.gatech.i3l.fhir.jpa.query.AbstractPredicateBuilder;
 import edu.gatech.i3l.fhir.jpa.query.PredicateBuilder;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.From;
+import javax.persistence.criteria.Path;
+import javax.persistence.criteria.Predicate;
+
+@Transactional(propagation = Propagation.REQUIRED)
 public class OrganizationFhirResourceDao extends BaseFhirResourceDao<Organization> {
 
 	public OrganizationFhirResourceDao() {
@@ -15,7 +24,34 @@ public class OrganizationFhirResourceDao extends BaseFhirResourceDao<Organizatio
 	
 	@Override
 	public PredicateBuilder getPredicateBuilder() {
-		return new AbstractPredicateBuilder() {			
+		return new AbstractPredicateBuilder() {
+			@Override
+			public Predicate translatePredicateString(Class<? extends IResourceEntity> entity, String theParamName,
+                    String likeExpression, From<? extends IResourceEntity, ? extends IResourceEntity> from,
+                    CriteriaBuilder theBuilder) {
+			    switch ( theParamName ) {
+                    case Organization.SP_NAME:
+                        final Path location = from.get("location");
+                        if (location != null)
+                        {
+                            final Predicate lc1 = theBuilder.like(from.get("location").get("address1").as(String.class), likeExpression);
+                            final Predicate lc2 = theBuilder.like(from.get("location").get("address2").as(String.class), likeExpression);
+                            final Predicate lc3 = theBuilder.like(from.get("location").get("city").as(String.class), likeExpression);
+                            final Predicate lc4 = theBuilder.like(from.get("location").get("state").as(String.class), likeExpression);
+                            final Predicate lc5 = theBuilder.like(from.get("location").get("zipCode").as(String.class), likeExpression);
+                            final Predicate lc6 = theBuilder.like(from.get("location").get("country").as(String.class), likeExpression);
+                            final Predicate lc = theBuilder.or(lc1, lc2, lc3, lc4, lc5, lc6);
+                            final Predicate name = theBuilder.like(from.get("careSiteName").as(String.class), likeExpression);
+                            return theBuilder.and(name,lc);
+                        }
+                        else
+                        {
+                            return theBuilder.like(from.get("careSiteName").as(String.class), likeExpression);
+                        }
+			    	default:
+						return super.translatePredicateString(entity, theParamName, likeExpression, from, theBuilder);
+				}
+			}
 		};
 	}
 
